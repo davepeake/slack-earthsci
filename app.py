531523eb965a164f4cda756953e8f7f2d1443a16
David Peake <david.peake@detectaustralia.com.au>
2018-10-06 20:33:02 +1000

    Still going

--- app.py
+++ app.py
@@ -14,17 +14,21 @@ verificiation_token=os.environ['VERIFICATION_TOKEN']
 def hello():
     return "Earth Sci Weather for Slack"
 
-@app.route('/earthsci', methods=['GET','POST'])
+@app.route('/earthsci', methods=['GET'])
 def earthsci():
     # do the thing
     # grab the data
+    '''
     if request.form['token'] == verification_token:
-        q = requests.get('http://128.250.56.142:8000/current-weather/').json()
+    '''
+    #q = requests.get('http://128.250.56.142:8000/current-weather/').json()
 
-        ret_str = 'The current temperature is %0.1f degrees, with a humidity of %0.1f%% with a wind speed of %0.1f kph.' % (q['curr_temp'], q['curr_humid'], q['curr_wind_spd'])
-        ret = {'text': ret_str}
+    #ret_str = 'The current temperature is %0.1f degrees, with a humidity of %0.1f%% with a wind speed of %0.1f kph.' % (q['curr_temp'], q['curr_humid'], q['curr_wind_spd'])
+    ret_str = 'Testing'
+    ret = { "response_type": "emphemeral",
+            'text': ret_str}
 
-        return jsonify(ret)
+    return jsonify(ret)
 
 if __name__ == '__main__':
     app.run()
