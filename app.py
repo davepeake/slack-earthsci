d1b7d38efdc7e45ad1df3661d43e81dc3e1a6fdf
David Peake <david.peake@detectaustralia.com.au>
2018-10-06 19:58:54 +1000

    Added requests and fixed up app

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
 from flask import Flask
+from flask import request
+from flask import jsonify
+
+import requests
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return "Earth Sci Weather for Slack"
+
+@app.route('/earthsci', methods=['GET'])
+def earthsci():
+    # do the thing
+    # grab the data
+    q = requests.get('http://128.250.56.142:8000/current-weather/').json()
+
+    ret_str = 'The current temperature is %0.1f degrees, with a humidity of %0.1f%% with a wind speed of %0.1f kph.' % (q['curr_temp'], q['curr_humid'], q['curr_wind_spd'])
+    ret = {'text': ret_str}
+
+    return jsonify(ret)
 
 if __name__ == '__main__':
     app.run()
